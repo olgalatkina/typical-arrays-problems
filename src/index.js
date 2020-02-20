efa51e496ed585eb77135e8f9d6709c6ab547feb
Olga Latkina <olga.latkina@gmail.com>
2020-02-20 20:11:24 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let result = array[0];
+
+        array.forEach(element => {
+            if (result > element) {
+                result = element;
+            }
+        });
+
+        return result;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let result = array[0];
+
+        array.forEach(element => {
+            if (result < element) {
+                result = element;
+            }
+        });
+
+        return result;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let result = array.reduce((sum, current) => sum + current, 0);
+        return result / array.length;
+    }
 }
